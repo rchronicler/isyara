4eb24a793441c9988a5291f9d042aad9086f3361
Dimas Andrian <dimas@gnuweeb.org>
2024-11-20 11:53:36 +0700

    fix: Add default value to user role

--- database/migrations/2024_11_20_042537_create_users_table.php
+++ database/migrations/2024_11_20_042537_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->enum('role', ['regular', 'admin', 'volunteer'])->nullable();
+            $table->enum('role', ['regular', 'admin', 'volunteer'])->nullable()->default('regular');
         });
     }
 
