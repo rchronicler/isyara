0d56d8097dbf31822cce782231abc00e15efcc09
Dimas Andrian <dimas@gnuweeb.org>
2024-11-23 17:03:16 +0700

    chore: Change seed data to Indonesian

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -6,77 +6,79 @@ use Illuminate\Database\Seeder;
 
 class CategoriesTableSeeder extends Seeder
 {
-
     /**
-     * Auto generated seed file
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        
-
         \DB::table('categories')->delete();
-        
-        \DB::table('categories')->insert(array (
-            0 => 
-            array (
-                'category_id' => 1,
-                'category_name' => 'Vocabulary',
-                'created_at' => '2024-11-20 11:20:08',
-                'updated_at' => '2024-11-20 11:20:08',
-            ),
-            1 => 
-            array (
-                'category_id' => 2,
-                'category_name' => 'Grammar',
-                'created_at' => '2024-11-20 11:20:08',
-                'updated_at' => '2024-11-20 11:20:08',
-            ),
-            2 => 
-            array (
-                'category_id' => 3,
-                'category_name' => 'Phrases',
-                'created_at' => '2024-11-20 11:20:08',
-                'updated_at' => '2024-11-20 11:20:08',
-            ),
-            3 => 
-            array (
-                'category_id' => 4,
-                'category_name' => 'Idioms',
-                'created_at' => '2024-11-20 11:20:08',
-                'updated_at' => '2024-11-20 11:20:08',
-            ),
-            4 => 
-            array (
-                'category_id' => 5,
-                'category_name' => 'Common Mistakes',
-                'created_at' => '2024-11-20 11:20:08',
-                'updated_at' => '2024-11-20 11:20:08',
-            ),
-            5 => 
-            array (
-                'category_id' => 6,
-                'category_name' => 'Slang',
-                'created_at' => '2024-11-20 11:20:08',
-                'updated_at' => '2024-11-20 11:20:08',
-            ),
-            6 => 
-            array (
-                'category_id' => 7,
-                'category_name' => 'Formal Expressions',
-                'created_at' => '2024-11-20 11:20:08',
-                'updated_at' => '2024-11-20 11:20:08',
-            ),
-            7 => 
-            array (
-                'category_id' => 8,
-                'category_name' => 'Informal Expressions',
-                'created_at' => '2024-11-20 11:20:08',
-                'updated_at' => '2024-11-20 11:20:08',
-            ),
+
+        \DB::table('categories')->insert(array(
+            0 =>
+                array(
+                    'category_id' => 1,
+                    'category_name' => 'Kata Benda',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ),
+            1 =>
+                array(
+                    'category_id' => 2,
+                    'category_name' => 'Kata Kerja',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ),
+            2 =>
+                array(
+                    'category_id' => 3,
+                    'category_name' => 'Kata Sifat',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ),
+            3 =>
+                array(
+                    'category_id' => 4,
+                    'category_name' => 'Kata Keterangan',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ),
+            4 =>
+                array(
+                    'category_id' => 5,
+                    'category_name' => 'Kata Ganti',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ),
+            5 =>
+                array(
+                    'category_id' => 6,
+                    'category_name' => 'Kata Sambung',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ),
+            6 =>
+                array(
+                    'category_id' => 7,
+                    'category_name' => 'Kata Depan',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ),
+            7 =>
+                array(
+                    'category_id' => 8,
+                    'category_name' => 'Kata Seru',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ),
+            8 =>
+                array(
+                    'category_id' => 9,
+                    'category_name' => 'Istilah Khusus',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ),
         ));
-        
-        
     }
 }
